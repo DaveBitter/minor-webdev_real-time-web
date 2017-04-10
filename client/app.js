4c813603b6bc7e95211488b42d488735bccf8230
d.bitter <d.bitter@viriciti.nl>
2017-04-10 14:33:37 +0200

    no inline scropts

--- client/app.js
+++ client/app.js
@@ -1,1 +1,12 @@
-var socket = io();
+$(function() {
+	var socket = io();
+	$('form').submit(function() {
+		socket.emit('chat message', $('#m').val());
+		$('#m').val('');
+		return false;
+	});
+	socket.on('chat message', function(msg) {
+		$('#messages').append($('<li>').text(msg));
+		window.scrollTo(0, document.body.scrollHeight);
+	});
+});
--- public/js/build.js
+++ public/js/build.js
@@ -1,3 +1,14 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var socket = io();
+$(function() {
+	var socket = io();
+	$('form').submit(function() {
+		socket.emit('chat message', $('#m').val());
+		$('#m').val('');
+		return false;
+	});
+	socket.on('chat message', function(msg) {
+		$('#messages').append($('<li>').text(msg));
+		window.scrollTo(0, document.body.scrollHeight);
+	});
+});
 },{}]},{},[1]);
