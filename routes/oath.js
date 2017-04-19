8a8bec63589eb1796c956ece7a14b3ac278e2b0b
d.bitter <daveybitter@gmail.com>
2017-04-19 14:55:53 +0200

    added basic styling so it doesnt look fugly

--- routes/oath.js
+++ routes/oath.js
@@ -5,12 +5,12 @@ const request = require('request')
 
 router.get('/', (req, res) => {
 	const client_id = process.env.CLIENTID
-const client_secret = process.env.CLIENTSECRET
-const redirect_uri = process.env.REDIRECTURI
+	const client_secret = process.env.CLIENTSECRET
+	const redirect_uri = process.env.REDIRECTURI
 
-	var url = "https://api.instagram.com/oauth/access_token";
-	var code = req.query.code
-	var options = {
+	const url = "https://api.instagram.com/oauth/access_token";
+	const code = req.query.code
+	const options = {
 		url: url,
 		method: "POST",
 		form: {
@@ -24,8 +24,13 @@ const redirect_uri = process.env.REDIRECTURI
 	}
 
 	request(options, function(err, response, body) {
+		if (body.code == 400) {
+			res.redirect('/')
+		}
 		const user = body.user
-		res.render('templates/profile', {user})
+		res.render('templates/profile', {
+			user
+		})
 	})
 })
 
